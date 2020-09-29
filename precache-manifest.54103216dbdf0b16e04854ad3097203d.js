392be28a1bc7a055a1f9e504a197545c8539e559
Mavriche Raluca <ralucamavriche@gmail.com>
2020-09-29 18:45:24 +0300

    Updates

---
+++ precache-manifest.54103216dbdf0b16e04854ad3097203d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958901303fb7d094c6fcf91ae89922b7",
+    "url": "/ralucamavriche/DesignTest/index.html"
+  },
+  {
+    "revision": "f3516d63ba7116d5a81e",
+    "url": "/ralucamavriche/DesignTest/static/css/main.349b955e.chunk.css"
+  },
+  {
+    "revision": "3bbe6a6178701da6204c",
+    "url": "/ralucamavriche/DesignTest/static/js/2.77030c92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ralucamavriche/DesignTest/static/js/2.77030c92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3516d63ba7116d5a81e",
+    "url": "/ralucamavriche/DesignTest/static/js/main.f44d0188.chunk.js"
+  },
+  {
+    "revision": "3965c546e348c7198b13",
+    "url": "/ralucamavriche/DesignTest/static/js/runtime-main.03a71450.js"
+  }
+]);
